ab4e2f93caf01494c9d4adf24b8d63350f75e4ce
Jeremy Sapsed <dev@jsapsed.co.uk>
2024-01-27 20:57:32 +0000

    Added config item which will add local currency and amount columns to exported csv.

--- src/app.js
+++ src/app.js
@@ -1,19 +1,29 @@
 // Load .env values into process.env
 import dotenv from "dotenv";
+
 dotenv.config();
 
 // Import required libraries.
 import express from "express";
-import { writeFile, copyFile, mkdir } from 'fs/promises';
-import { parse } from 'url';
-import { stringify } from 'csv-stringify';
+import {writeFile, copyFile, mkdir} from 'fs/promises';
+import {parse} from 'url';
+import {stringify} from 'csv-stringify';
 import format from 'date-format';
 import MonzoClient from "./MonzoClient.js";
 
 const hostname = process.env.HOSTNAME;
 const port = Number(process.env.PORT);
 const server = express();
-const includeTime = process.env.INCLUDE_TIME.toLowerCase() == "true";
+const includeTime = process.env.INCLUDE_TIME?.toLowerCase() === "true";
+const includeLocalCurrency = process.env.INCLUDE_LOCAL_CURRENCY?.toLowerCase() === "true";
+
+if (includeTime) {
+    console.log("Time export enabled.");
+}
+
+if (includeLocalCurrency) {
+    console.log("Local currency export enabled");
+}
 
 const OUTPUT_FILENAME = process.env.OUTPUT_FILENAME
 
@@ -40,27 +50,27 @@ server.get('/auth/callback', async (req, res) => {
 
     const code = queryObject.code;
     const state = queryObject.state;
-    
+
     try {
         client.GetAccessToken(code, state);
     } catch (error) {
         return res.status(400).send(error.message);
     }
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Try adding some query parameters to the end of the url.');
     // res.send('Try adding some query parameters to the end of the url.');
 })
 
 server.get('/whoami', async (req, res) => {
     const data = await client.WhoAmI();
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(data));
 })
 
 server.get('/accounts', async (req, res) => {
     const data = await client.GetAccounts();
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(data));
 })
 
@@ -68,18 +78,18 @@ server.get('/transactions', async (req, res) => {
     const queryObject = parse(req.url, true).query;
     const data = await client.GetTransactions(queryObject.since);
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(data));
 })
 
 server.get('/transactions/latest', async (req, res) => {
     const date = new Date();
     date.setDate(date.getDate() - 5);
-    date.setHours(0, 0, 0 ,0);
+    date.setHours(0, 0, 0, 0);
     const since = date.toISOString();
 
     const data = await client.GetTransactions(since);
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(data));
 })
 
@@ -88,22 +98,22 @@ server.get('/export/transactions', async (req, res) => {
     const data = await client.GetTransactions(queryObject.since);
     TransactionsToCsv(TransformTransactions(data.transactions));
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ success: true }));
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({success: true}));
 })
 
 server.get('/export/transactions/latest', async (req, res) => {
     const date = new Date();
     date.setDate(date.getDate() - 5);
-    date.setHours(0, 0, 0 ,0);
+    date.setHours(0, 0, 0, 0);
     const since = date.toISOString();
     console.log(since);
 
     const data = await client.GetTransactions(since);
     TransactionsToCsv(TransformTransactions(data));
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ success: true }));
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({success: true}));
 })
 
 server.listen(port, hostname, () => {
@@ -114,7 +124,7 @@ function TransformTransactions(transactions) {
     return transactions.map((transaction) => {
         const createdDate = new Date(transaction.created);
         const regex = /\s+/g
-        return {
+        const row = {
             account_id: transaction.account_id,
             id: transaction.id,
             date: format(includeTime ? "yyyy/MM/dd hh:mm:ss" : "yyyy/MM/dd", createdDate),
@@ -127,13 +137,20 @@ function TransformTransactions(transactions) {
             currency: transaction.currency,
             notes: transaction.notes,
             description: transaction.description.replace(regex, ' ')
+        };
+
+        if (includeLocalCurrency) {
+            row.local_amount = transaction.local_amount / 100;
+            row.local_currency = transaction.local_currency;
         }
+
+        return row;
     });
 }
 
 function TransactionsToCsv(transactions) {
     console.log("Converting to csv");
-    stringify(transactions, { header: true }, async (err, output) => {
+    stringify(transactions, {header: true}, async (err, output) => {
         if (err) {
             console.error(err);
             return;
@@ -151,8 +168,8 @@ function TransactionsToCsv(transactions) {
 
 async function Setup() {
     // Create directories
-    await mkdir(OUTPUT_DIRECTORY, { recursive: true });
-    await mkdir(ARCHIVE_DIRECTORY, { recursive: true });
+    await mkdir(OUTPUT_DIRECTORY, {recursive: true});
+    await mkdir(ARCHIVE_DIRECTORY, {recursive: true});
 
     await client.Init();
 }
